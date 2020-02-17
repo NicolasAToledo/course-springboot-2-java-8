c963df32e03f1e7f0df74847e551f5f0b1c050f3
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2020-02-17 11:04:32 -0300

    Service layer, component registration

--- src/main/java/com/fatecmc/course/repositories/UserRepository.java
+++ src/main/java/com/fatecmc/course/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.fatecmc.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fatecmc.course.entities.User;
-
+//OPICIONAL USAR @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
