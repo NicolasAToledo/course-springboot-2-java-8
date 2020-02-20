0dd8e9962a68a8fa04478a5c0a5c22f631dfe9ff
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2020-02-20 01:49:55 -0300

    Payment, one-to-one association

--- src/main/java/com/fatecmc/course/config/TestConfig.java
+++ src/main/java/com/fatecmc/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fatecmc.course.entities.Category;
 import com.fatecmc.course.entities.Order;
 import com.fatecmc.course.entities.OrderItem;
+import com.fatecmc.course.entities.Payment;
 import com.fatecmc.course.entities.Product;
 import com.fatecmc.course.entities.User;
 import com.fatecmc.course.entities.enums.OrderStatus;
@@ -79,5 +80,8 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);//apos setar um pagamento para um pedido, vc salva novamente e o JPS vai Salvar pedido no banco.
 	}
 }
